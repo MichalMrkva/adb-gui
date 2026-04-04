f64d4ad69afc4b2e11daddeaa030fb774451798f
Michal Mrkva <mrkva.michal@gmail.com>
2026-04-04 11:28:07 +0200

    adb interface implementation

--- composeApp/src/jvmMain/kotlin/cz/uuk/adbgui/data/AdbRepo.kt
+++ composeApp/src/jvmMain/kotlin/cz/uuk/adbgui/data/AdbRepo.kt
@@ -1,45 +1,40 @@
 package cz.uuk.adbgui.data
 
 import cz.uuk.adbgui.domain.model.Device
+import cz.uuk.adbgui.utils.tickerFlow
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.*
-import kotlin.time.Duration
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlin.time.Duration.Companion.seconds
 
 class AdbRepository(
     private val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
 ) {
-
-    private val _devices = MutableStateFlow<List<Device>>(emptyList())
-    val devices: StateFlow<List<Device>> = _devices
+    val devices: StateFlow<List<Device>> = devicesStateFlow()
 
     private fun startServer() {
         getDevices()
     }
 
-    private fun getDevices() {}
+    private fun devicesStateFlow() = tickerFlow(1.seconds, 0.seconds).map {
+        getDevices()
+    }.stateIn(scope, SharingStarted.Lazily, emptyList())
+
+    private fun getDevices(): List<Device> {
+        return emptyList()
+    }
 
     private fun getPackages(device: Device): List<Package> {
         return emptyList()
     }
 
-    suspend fun devicePackages(device: Device): StateFlow<List<Package>> = tickerFlow(1.seconds).map {
+    fun devicePackages(device: Device): StateFlow<List<Package>> = tickerFlow(1.seconds, 0.seconds).map {
         getPackages(device)
-    }.stateIn(scope)
+    }.stateIn(scope, SharingStarted.WhileSubscribed(1.seconds.inWholeMilliseconds), emptyList())
 
 
-    fun deletePackage(device: Device, pack: Package) {
-
-    }
-}
-
-
-fun tickerFlow(period: Duration, initialDelay: Duration = period) = flow {
-    delay(initialDelay)
-    while (true) {
-        emit(Unit)
-        delay(period)
-    }
+    fun deletePackage(device: Device, pack: Package) {}
 }
---
+++ composeApp/src/jvmMain/kotlin/cz/uuk/adbgui/utils/TickerFlow.kt
@@ -0,0 +1,13 @@
+package cz.uuk.adbgui.utils
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlin.time.Duration
+
+fun tickerFlow(period: Duration, initialDelay: Duration = period) = flow {
+    delay(initialDelay)
+    while (true) {
+        emit(Unit)
+        delay(period)
+    }
+}
