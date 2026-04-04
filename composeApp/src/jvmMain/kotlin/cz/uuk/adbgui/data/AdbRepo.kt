053e292a9091afc8c3d88fe8ceccb4c1bd2b0625
Michal Mrkva <mrkva.michal@gmail.com>
2026-04-04 11:16:06 +0200

    adb repo ticker flow

--- composeApp/src/jvmMain/kotlin/cz/uuk/adbgui/data/AdbRepo.kt
+++ composeApp/src/jvmMain/kotlin/cz/uuk/adbgui/data/AdbRepo.kt
@@ -1,10 +1,17 @@
 package cz.uuk.adbgui.data
 
 import cz.uuk.adbgui.domain.model.Device
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
-class AdbRepository {
+class AdbRepository(
+    private val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
+) {
+
     private val _devices = MutableStateFlow<List<Device>>(emptyList())
     val devices: StateFlow<List<Device>> = _devices
 
@@ -14,8 +21,25 @@ class AdbRepository {
 
     private fun getDevices() {}
 
-    fun devicePackages(device: Device): StateFlow<List<Package>> {
-        val flow = MutableStateFlow<List<Package>>(emptyList())
-        return flow
+    private fun getPackages(device: Device): List<Package> {
+        return emptyList()
+    }
+
+    suspend fun devicePackages(device: Device): StateFlow<List<Package>> = tickerFlow(1.seconds).map {
+        getPackages(device)
+    }.stateIn(scope)
+
+
+    fun deletePackage(device: Device, pack: Package) {
+
+    }
+}
+
+
+fun tickerFlow(period: Duration, initialDelay: Duration = period) = flow {
+    delay(initialDelay)
+    while (true) {
+        emit(Unit)
+        delay(period)
     }
 }
